0e523107a9255608f4694bf27a28e533bc5ec269
liulong <1161515052@qq.com>
2017-11-27 00:02:39 +0800

    xlrd

--- test.py
+++ test.py
@@ -14,3 +14,35 @@ os.remove("111.txt")
 os.getcwd()
 #获取指定目录中的文件和文件夹
 os.listdir(".")
+
+#coding:utf-8
+from xml.etree.ElementTree import parse
+f = open('C:\\Users\\Administrator\\Desktop\\demo.xml')
+et = parse(f)
+root = et.getroot()
+list = []
+for child in root:
+	for detail in child.iter('constant'):
+		result = {}
+		result['name'] = detail.find('name').text
+		result['age'] = detail.find('age').text
+		result['school'] = detail.find('school').text
+		list.append(result)
+print list
+===========================================
+#coding:utf-8
+import xlrd
+book = xlrd.open_workbook('C:\\Users\\Administrator\\Desktop\\demo.xlsx')
+table = book.sheet_by_index(0)
+nrows = table.nrows
+#ncols = table.ncols
+list = []
+for rownum in range(1,nrows):
+	row = table.row_values(rownum)
+	if row:
+		result = {}
+		result['name'] = row[0]
+		result['age'] = row[1]
+		result['school'] = row[2]
+		list.append(result)
+print list
