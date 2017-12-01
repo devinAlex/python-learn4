28746595aacec30b95ffc7cbad8fdd2bf2235efb
liulong <1161515052@qq.com>
2017-12-02 00:20:44 +0800

    ftp

--- test.py
+++ test.py
@@ -46,3 +46,73 @@ for rownum in range(1,nrows):
 		result['school'] = row[2]
 		list.append(result)
 print list
+/////////////////////////////////////
+import os
+from ftplib import FTP, error_perm
+class FTPUtils(object):
+    def __init__(self, host):
+        self.ftp = FTP()
+        self.ftp.connect(host, 21)
+        print '***Connected to host "%s"' % host
+
+    #登录    
+    def login(self, user, password):
+        try:
+            self.ftp.login(user, password)
+        except error_perm:
+            print 'ERROR: cannot login as %' % user
+            self.ftp.quit()
+            return
+        print '***Logged in as %s' % user
+
+    #移动到某个目录下
+    def cwd(self, where):
+        try:
+            self.ftp.cwd(where)
+        except error_perm:
+            print 'ERRORL cannot CD to "%s"' % where
+            self.ftp.quit()
+            return
+        print '*** Changed to "%s" folder' % where
+
+    #下载文件
+    def get(self, local):
+        try:
+            self.ftp.retrbinary('RETR %s' % local, open(local, 'wb').write)
+            print "start"
+        except error_perm:
+            print 'ERROR: cannot read file "%s"' % local
+            os.unlink(local)
+        else:
+            print '*** Downloaded "%s" to CWD' % local
+            self.ftp.quit()
+        print "end"
+        return
+
+    #列出当前目录
+    def dir(self):
+        self.ftp.dir()
+
+    #下载当前目录所有文件
+    def down_all(self, path):
+        list = self.ftp.nlst()
+        for l in list:
+            self.ftp.retrbinary('RETR %s' % l, open(path + "/" + l, 'wb').write)
+        return
+
+    #关闭ftp连接
+    def close(self):
+        self.ftp.close()
+
+#测试
+if __name__ == "__main__":
+    port = 22
+    host = "xxx.cn"
+    user = "xxx"
+    password = "xxx"
+    test = FTPUtils(host)
+    test.login(user, password)
+    test.cwd("application/logs")
+    os.mkdir("logs")
+    test.down_all("logs/")
+    test.close()
